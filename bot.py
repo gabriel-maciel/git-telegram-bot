ad450191cff42dde609d4d6677893c2a531d8794
Gabriel Maciel <g.maciel@buenosaires.gob.ar>
2023-06-01 18:29:42 -0300

    Change message formate

--- bot.py
+++ bot.py
@@ -10,15 +10,12 @@ bot = Bot(token=TELEGRAM_TOKEN)
 
 @app.post('/webhook')
 async def gitlab_webhook(request: Request):
-    print("hello?")
-    await bot.send_message(chat_id=os.getenv('TELEGRAM_CHAT_ID'), text='Webhook received')
-
     json = await request.json()
     ref = json['ref']
     if 'master' in ref or 'dev' in ref:
         commits = json['commits']
         for commit in commits:
-            message = f"Nuevo commit en {ref}: {commit['message']} por {commit['author']['name']}"
+            message = f'Nuevo commit en {ref}: \"{commit["message"]}\" por {commit["author"]["name"]}'
             print(message)
             await bot.send_message(chat_id=os.getenv('TELEGRAM_CHAT_ID'), text=message)
 
