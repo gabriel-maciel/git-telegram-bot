5ff14203aece498c0506e807dee50110239c8bc0
Gabriel Maciel <g.maciel@buenosaires.gob.ar>
2023-06-02 09:53:15 -0300

    Add MR webhook

--- bot.py
+++ bot.py
@@ -1,25 +1,52 @@
 import os
-from fastapi import FastAPI
-from telegram import Bot
-from starlette.requests import Request
+from typing import List
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
+from aiogram import Bot, types
+
+class Author(BaseModel):
+    name: str
+    email: str
+
+class Commit(BaseModel):
+    id: str
+    message: str
+    timestamp: str
+    url: str
+    author: Author
+
+class WebhookCommit(BaseModel):
+    commits: List[Commit]
+    ref: str
+
+class MR(BaseModel):
+    id: int
+    target_branch: str
+    source_branch: str
+    url: str
+
+class WebhookMR(BaseModel):
+    object_attributes: MR
 
 app = FastAPI()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 bot = Bot(token=TELEGRAM_TOKEN)
 
-@app.post('/webhook')
-async def gitlab_webhook(request: Request):
-    json = await request.json()
-    ref = json['ref']
-    if 'master' in ref or 'dev' in ref:
-        commits = json['commits']
-        for commit in commits:
-            message = f'Nuevo commit en {ref}: \"{commit["message"]}\" por {commit["author"]["name"]}'
-            print(message)
-            await bot.send_message(chat_id=os.getenv('TELEGRAM_CHAT_ID'), text=message)
+@app.post("/webhook/commit")
+async def gitlab_webhook_commit(webhook: WebhookCommit):
+    for commit in webhook.commits:
+        message = f"Nuevo commit en {webhook.ref}: '{commit.message}' por {commit.author.name}. URL: {commit.url}"
+        await bot.send_message(chat_id=CHAT_ID, text=message)
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "OK"})
 
-    return {"message": "OK"}
+@app.post("/webhook/mr")
+async def gitlab_webhook_mr(webhook: WebhookMR):
+    if webhook.object_attributes.target_branch == 'dev':
+        message = f"Nuevo Merge Request: '{webhook.object_attributes.source_branch}' a 'dev'. URL: {webhook.object_attributes.url}"
+        await bot.send_message(chat_id=CHAT_ID, text=message)
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "OK"})
 
 @app.post("/webhook-echo")
 async def gitlab_webhook_echo():    
